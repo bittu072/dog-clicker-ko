e0558c464ae4c4e23a3e3fc68235c0fee83d2a13
bittu072 <bittu.patel072@gmail.com>
2017-02-09 01:29:03 -0800

    updated list of dogs

--- js/dogscript.js
+++ js/dogscript.js
@@ -1,9 +1,9 @@
-var Dog = function() {
-    this.ClickCount = ko.observable(0);
-    this.name = ko.observable('Scooby');
-    this.dogImg = ko.observable('img/img1.jpg');
-    this.nickname = ko.observableArray(['Superman', 'Spiderman', 'Batman'])
-    this.buffer = ko.observable('dog');
+var Dog = function(data) {
+    this.ClickCount = ko.observable(data.ClickCount);
+    this.name = ko.observable(data.name);
+    this.dogImg = ko.observable(data.dogImg);
+    this.nickname = ko.observableArray(data.nickname);
+    this.buffer = ko.observable("doggie");
 
 
     this.bufferfunction = ko.computed(function() {
@@ -17,9 +17,60 @@ var Dog = function() {
     }, this);
 }
 
+var initialDogs = [
+    {
+            ClickCount: 0,
+            name: 'Scooby',
+            dogImg: 'img/img1.jpg',
+            nickname: ['Superman', 'Spiderman']
+        },
+        {
+            ClickCount: 0,
+            name: 'Popoye',
+            dogImg: 'img/img2.jpg',
+            nickname: ['Flash', 'Aquaman']
+        },
+        {
+            ClickCount: 0,
+            name: 'Tom',
+            dogImg: 'img/img3.jpg',
+            nickname: ['Iron-man']
+        },
+        {
+            ClickCount: 0,
+            name: 'Ruffy',
+            dogImg: 'img/img4.jpg',
+            nickname: ['Thor']
+        },
+        {
+            ClickCount: 0,
+            name: 'Blue',
+            dogImg: 'img/img5.jpg',
+            nickname: ['Batman']
+        },
+        {
+            ClickCount: 0,
+            name: 'Augie',
+            dogImg: 'img/img6.jpg',
+            nickname: ['captain']
+        },
+        {
+            ClickCount: 0,
+            name: 'Bruno',
+            dogImg: 'img/img7.jpg'
+        }
+]
+
 var ViewModel = function() {
     var self = this;
-    this.currentDog = ko.observable( new Dog() );
+
+    this.dogList = ko.observableArray([]);
+
+    initialDogs.forEach(function(dogItem){
+        self.dogList.push(new Dog(dogItem));
+    });
+
+    this.currentDog = ko.observable( this.dogList()[0] );
 
     this.incrementCounter = function() {
         self.currentDog().ClickCount(self.currentDog().ClickCount() +1);
