040e5a293911a13ab52a17f47f1f3b020cc603a7
bittu072 <bittu.patel072@gmail.com>
2017-02-09 00:41:34 -0800

    added dog popularity level in dog clicker app based on clickcount using knockout JS

--- js/dogscript.js
+++ js/dogscript.js
@@ -2,14 +2,23 @@ var ViewModel = function() {
     this.ClickCount = ko.observable(0);
     this.name = ko.observable('Scooby');
     this.dogImg = ko.observable('img/img1.jpg');
+    this.nickname = ko.observableArray(['Superman', 'Spiderman', 'Batman'])
+    this.buffer = ko.observable('dog');
+
+
+    this.bufferfunction = ko.computed(function() {
+        if (this.ClickCount() > 20) {
+            return this.buffer('People like this dog');
+        } else if (this.ClickCount() > 10){
+            return this.buffer('People are crazy for this dog');
+        } else {
+            return this.buffer('doggie');
+        }
+    }, this);
 
     this.incrementCounter = function() {
         this.ClickCount(this.ClickCount() +1);
     };
-
-    this.hasClickedTooManyTimes = ko.pureComputed(function() {
-        return this.ClickCount() > 5;
-    }, this);
 
 }
 
